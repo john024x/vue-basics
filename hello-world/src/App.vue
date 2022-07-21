b15a947e49626016786087d73a443b19303fc1e9
Juan Fuentes <juanfuentes@Juans-MacBook-Air-2.local>
2022-07-21 16:07:42 -0500

    binding classes

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -1,7 +1,15 @@
 <template>
   <div>{{ name }}</div>
   <div v-html="channel"></div>
-  <h2 v-bind:id="headingID">Heading</h2>
+  <h2 class="underline" v-bind:id="headingID">Heading</h2>
+  <h3 class ="underline" v-bind:class="success">Heading 3</h3>
+  <p v-bind:class="['underline', 'successClass']">New 3</p>
+  <p v-bind:class="isDisabled ? 'unsuccessClass' : 'successClass' ">New 4</p>
+  <h4 v-bind:class="{
+    'underline': true,
+    'success': isDisabled,
+    'unsuccess': !isDisabled
+  }">New 5</h4>
   <button v-bind:disabled="isDisabled">Button</button>
 </template>
 
@@ -15,7 +23,8 @@ export default {
       name: 'Hello World',
       channel: '<span>Channel</span>',
       headingID: 'heading',
-      isDisabled: true
+      isDisabled: false,
+      success: 'unsuccessClass'
     }
   },
   }
@@ -29,5 +38,14 @@ export default {
   text-align: center;
   color: #1e7ddb;
   margin-top: 60px;
+}
+.underline{
+  text-decoration: underline;
+}
+.successClass{
+  color: green;
+}
+.unsuccessClass{
+  color: firebrick;
 }
 </style>
